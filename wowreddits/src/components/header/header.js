523ecb3b17f4a7e8e16813f931c7a779f4efa3fc
ajjonesx3 <ajjonesx3@gmail.com>
2024-11-17 19:59:33 -0500

    modified header, includes logos

--- wowreddits/src/components/header/header.js
+++ wowreddits/src/components/header/header.js
@@ -1,10 +1,26 @@
 import './header.css';
+import redditLogo from '../../assets/reddit.svg';
+import wowLogo from '../../assets/wow.svg';
 
 const Header = () => {
 
+    const logoSize = {
+        width: "13em",
+        height: "auto"
+    }
+    const logoSize2 = {
+        width:"5.85em",
+        height:"auto",
+        padding:"24px 50px"
+    }
+
     return (
         <div className="header">
+            <img src={redditLogo} alt="reddit logo" style={logoSize}/>
             <h1>Wow Reddits</h1>
+            <img src={wowLogo} alt="wow logo" style={logoSize2}/>
+
+
         </div>
     )
 }
